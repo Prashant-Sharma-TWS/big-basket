58acc3e079f8fda0098279a0bee817d7c014dc96
prashant-sharma-tws <ps142500@gmail.com>
2022-03-28 19:10:59 +0530

    Offer sections completed with carousel

--- src/Elements/Viewport_Bank.jsx
+++ src/Elements/Viewport_Bank.jsx
@@ -8,6 +8,9 @@ export const ViewportSliderElement = styled.div`
 `;
 
 export const OfferSection = styled.section`
+  .offer-top-margin {
+    margin: 30px 0 0;
+  }
   h2 {
     color: rgb(68, 68, 68);
     text-align: center;
@@ -31,9 +34,10 @@ export const OfferSection = styled.section`
       border: 1px solid #e1e1e1;
       @media screen and (max-width: 870px) {
         width: 48%;
+        margin-bottom: 10px;
       }
 
-      img {
+      > img {
         width: 100%;
       }
     }
@@ -43,5 +47,78 @@ export const OfferSection = styled.section`
     > div {
       width: 15%;
     }
+
+    @media screen and (max-width: 870px) {
+      > div {
+        width: 48%;
+      }
+    }
+  }
+
+  .wrapperer.fruits-veg {
+    > div {
+      width: 48%;
+    }
+    > div:nth-child(2) {
+      border: none;
+    }
+
+    > .small-wrapper {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: center;
+      > div {
+        width: 48%;
+        border: 1px solid #e1e1e1;
+        margin-bottom: 5px;
+        > img {
+          width: 100%;
+          padding: 10px;
+        }
+      }
+    }
+
+    @media screen and (max-width: 870px) {
+      > div {
+        width: 100%;
+      }
+    }
+  }
+
+  #carouselExampleControls {
+    width: 85%;
+    margin: 50px auto 0;
+    border: 1px solid #e1e1e1;
+
+    a.carousel-control-prev,
+    a.carousel-control-next {
+      width: 20px;
+      height: 20px;
+      top: 45%;
+      color: #e1e1e1;
+      padding: 15px 10px;
+      background-color: #777;
+      border: 1px solid #dedede;
+      opacity: 0.5;
+
+      :hover {
+        opacity: 0.7;
+      }
+    }
+
+    a.carousel-control-prev {
+      left: -15px;
+    }
+    a.carousel-control-next {
+      right: -15px;
+    }
+    .prev-icon-car {
+      border: solid transparent;
+    }
+  }
+
+  #carouselExampleControls.featured-slide {
+    margin: 0 auto;
   }
 `;
