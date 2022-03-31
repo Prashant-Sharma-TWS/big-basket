465094d49419e9abe07d6377e0e0b656a3a34b1b
ketanyadav8807 <ketanyadavbdh8807@gmail.com>
2022-03-31 14:44:51 +0530

    SingleProd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import "./App.css";
-import { Navbar } from "./Components/Navbar";
-import { Routers } from "./Routers/Routers";
+// import "./App.css";
+import { SingleProduct } from "./Components/productPages/SingleProduct";
+import { ThankyouPage } from "./Components/productPages/ThankyouPage";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Navbar />
-        <Routers />
-      </div>
+      <SingleProduct />
     </div>
   );
 }
