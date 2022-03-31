a4fa66471702d1d3be56215d66c2b78caf3474b5
prashant-sharma-tws <ps142500@gmail.com>
2022-03-31 17:05:18 +0530

    Read more added to home page

--- src/Components/productPages/SingleProduct.jsx
+++ src/Components/productPages/SingleProduct.jsx
@@ -1,57 +1,105 @@
-import React , {useState} from 'react'
-import style from  './singleprod.module.css';
+import React, { useState } from "react";
+import style from "./singleprod.module.css";
 export const SingleProduct = () => {
+  const [image, setImage] = useState(
+    "https://www.bigbasket.com/media/uploads/p/l/1213522_3-nivea-men-fresh-active-roll-on.jpg"
+  );
+  // const [setcontentBox, setSetContentBox] = useState(false);
 
-    const [image, setImage] = useState("https://www.bigbasket.com/media/uploads/p/l/1213522_3-nivea-men-fresh-active-roll-on.jpg");
-    // const [setcontentBox, setSetContentBox] = useState(false);
-    
   return (
     <div>
-        <div className={style.mainDiv}>
-            
-            <div className={style.left}>
-                Left
+      <div className={style.mainDiv}>
+        <div className={style.left}>Left</div>
+        <div className={style.right}>
+          <div className={style.rightKaLeftMain}>
+            <img alt="" src={image} className={style.posterImg} />
+            <div className={style.subImagesMainiv}>
+              <img
+                alt=""
+                src="https://www.bigbasket.com/media/uploads/p/l/1213522_3-nivea-men-fresh-active-roll-on.jpg"
+                className={style.subImg}
+                onClick={() =>
+                  setImage(
+                    "https://www.bigbasket.com/media/uploads/p/l/1213522_3-nivea-men-fresh-active-roll-on.jpg"
+                  )
+                }
+              />
+              <img
+                alt=""
+                src="https://www.bigbasket.com/media/uploads/p/l/1213522-2_2-nivea-men-fresh-active-roll-on.jpg"
+                className={style.subImg}
+                onClick={() =>
+                  setImage(
+                    "https://www.bigbasket.com/media/uploads/p/l/1213522-2_2-nivea-men-fresh-active-roll-on.jpg"
+                  )
+                }
+              />
+              <img
+                alt=""
+                src="https://www.bigbasket.com/media/uploads/p/l/1213522-4_2-nivea-men-fresh-active-roll-on.jpg"
+                className={style.subImg}
+                onClick={() =>
+                  setImage(
+                    "https://www.bigbasket.com/media/uploads/p/l/1213522-4_2-nivea-men-fresh-active-roll-on.jpg"
+                  )
+                }
+              />
+              <img
+                alt=""
+                src="https://www.bigbasket.com/media/uploads/p/l/1213522-7_2-nivea-men-fresh-active-roll-on.jpg"
+                className={style.subImg}
+                onClick={() =>
+                  setImage(
+                    "https://www.bigbasket.com/media/uploads/p/l/1213522-7_2-nivea-men-fresh-active-roll-on.jpg"
+                  )
+                }
+              />
             </div>
-            <div className={style.right}>
-                <div className={style.rightKaLeftMain}>
-                    <img src={image} className={style.posterImg}/>
-                    <div className={style.subImagesMainiv}>
-                        <img src='https://www.bigbasket.com/media/uploads/p/l/1213522_3-nivea-men-fresh-active-roll-on.jpg' className={style.subImg}
-                        onClick={()=> setImage("https://www.bigbasket.com/media/uploads/p/l/1213522_3-nivea-men-fresh-active-roll-on.jpg")}/>
-                        <img src='https://www.bigbasket.com/media/uploads/p/l/1213522-2_2-nivea-men-fresh-active-roll-on.jpg' className={style.subImg} 
-                        onClick={()=> setImage("https://www.bigbasket.com/media/uploads/p/l/1213522-2_2-nivea-men-fresh-active-roll-on.jpg")}/>
-                        <img src='https://www.bigbasket.com/media/uploads/p/l/1213522-4_2-nivea-men-fresh-active-roll-on.jpg' className={style.subImg} 
-                        onClick={()=> setImage("https://www.bigbasket.com/media/uploads/p/l/1213522-4_2-nivea-men-fresh-active-roll-on.jpg")}/>
-                        <img src='https://www.bigbasket.com/media/uploads/p/l/1213522-7_2-nivea-men-fresh-active-roll-on.jpg' className={style.subImg} 
-                        onClick={()=> setImage("https://www.bigbasket.com/media/uploads/p/l/1213522-7_2-nivea-men-fresh-active-roll-on.jpg")}/>
-                    </div>
-                </div>
-                <div className={style.rightKaRightMain}>
-                    <p>Nivea Men Fresh Active Roll On, 2x50 ml (MultiPack)</p>
-                    <p>MRP:</p>
-                    <div className={style.ratingDiv}>
-                        <p className={style.rating}>
-                            4.6 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 26 26"><path fill="#689f38" fill-rule="nonzero" d="M19.72 16.183l.9 5.254a2 2 0 0 1-2.902 2.108l-4.485-2.358a.5.5 0 0 0-.466 0l-4.485 2.358a2 2 0 0 1-2.902-2.108l.9-5.254-3.816-3.72A2 2 0 0 1 3.572 9.05l5.275-.767 2.36-4.78a2 2 0 0 1 3.586 0l2.36 4.78 5.275.767a2 2 0 0 1 1.108 3.411l-3.817 3.721z"></path></svg>
-                        </p>
-                        <div className={style.ratingCount}>60 Ratings</div>
-                    </div>
-                    <div className={style.basketButtonGroupMainDiv}>
-                        <input />
-                        <button className={style.addtobasketBtn}>ADD TO BASKET</button>
-                        <button className={style.saveBtn}>SAVE</button>
-                    </div>
-                    <div className={style.deleverytruckdiv}>
-                        <img src='https://flyclipart.com/thumb2/free-delivery-delivery-delivery-truck-icon-with-png-and-vector-695245.png'className={style.deleverytruckimg}/>
-                        <p className={style.deleverytimep}>Standard: Today 3:00PM - 7:30PM</p>
-                    </div>
-                    <div>
-                        <p className={style.packSize}>Pack Sizes</p>
-                    </div>
-                </div>
+          </div>
+          <div className={style.rightKaRightMain}>
+            <p>Nivea Men Fresh Active Roll On, 2x50 ml (MultiPack)</p>
+            <p>MRP:</p>
+            <div className={style.ratingDiv}>
+              <p className={style.rating}>
+                4.6{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  viewBox="0 0 26 26"
+                >
+                  <path
+                    fill="#689f38"
+                    fill-rule="nonzero"
+                    d="M19.72 16.183l.9 5.254a2 2 0 0 1-2.902 2.108l-4.485-2.358a.5.5 0 0 0-.466 0l-4.485 2.358a2 2 0 0 1-2.902-2.108l.9-5.254-3.816-3.72A2 2 0 0 1 3.572 9.05l5.275-.767 2.36-4.78a2 2 0 0 1 3.586 0l2.36 4.78 5.275.767a2 2 0 0 1 1.108 3.411l-3.817 3.721z"
+                  ></path>
+                </svg>
+              </p>
+              <div className={style.ratingCount}>60 Ratings</div>
             </div>
+            <div className={style.basketButtonGroupMainDiv}>
+              <input />
+              <button className={style.addtobasketBtn}>ADD TO BASKET</button>
+              <button className={style.saveBtn}>SAVE</button>
+            </div>
+            <div className={style.deleverytruckdiv}>
+              <img
+                alt=""
+                src="https://flyclipart.com/thumb2/free-delivery-delivery-delivery-truck-icon-with-png-and-vector-695245.png"
+                className={style.deleverytruckimg}
+              />
+              <p className={style.deleverytimep}>
+                Standard: Today 3:00PM - 7:30PM
+              </p>
+            </div>
+            <div>
+              <p className={style.packSize}>Pack Sizes</p>
+            </div>
+          </div>
         </div>
+      </div>
 
-        {/* <div className={style.accordion}>
+      {/* <div className={style.accordion}>
         <div className={({setcontentBox} ? style.active : style.contentBox)}>
         <div className={style.label}>Benefits</div>
         <div className={style.content}>
@@ -63,5 +111,5 @@ export const SingleProduct = () => {
         </div>
         </div> */}
     </div>
-  )
-}
+  );
+};
--- src/Elements/Viewport_Bank.jsx
+++ src/Elements/Viewport_Bank.jsx
@@ -151,6 +151,17 @@ export const OfferSection = styled.section`
     overflow-x: hidden;
     overflow-y: hidden;
 
+    @media screen and (max-width: 870px) {
+      min-height: 320px;
+
+      li.best-seller-product {
+        min-width: 150px;
+        max-width: 150px;
+        width: 150px;
+        background-color: orange;
+      }
+    }
+
     li.best-seller-product {
       min-width: 220px;
       width: 220px;
@@ -158,6 +169,12 @@ export const OfferSection = styled.section`
       margin: 0 4.5px;
       border: 1px solid #f2f2f2;
       background: #fff;
+
+      @media screen and (max-width: 870px) {
+        min-width: 198px;
+        max-width: 198px;
+        width: 198px;
+      }
     }
 
     .product-display {
@@ -170,6 +187,52 @@ export const OfferSection = styled.section`
       .price-details {
         min-height: 90px;
       }
+    }
+  }
+
+  .store-info-box {
+    width: 85%;
+    margin: 30px auto 0;
+    padding: 0 15px;
+    color: #333;
+    border: 1px solid #dfdada;
+
+    h3 {
+      margin-top: 20px;
+      font-size: 30px;
+    }
+    h4 {
+      font-size: 24px;
+    }
+    p {
+      color: #6b6868;
+      font-size: 13px;
+      line-height: 1.4;
+    }
+    b {
+      color: #50c71f;
+    }
+    ul {
+      li {
+        margin-left: 40px;
+        list-style-type: disc;
+        font-size: 13px;
+        line-height: 1.4;
+      }
+    }
+    button {
+      margin-bottom: 10px;
+      color: #6b6868;
+      border: 1px solid rgb(0, 0, 0, 0.3);
+      border-radius: 5px;
+      font-size: 13px;
+      padding: 5px 8px;
+      outline: none;
+    }
+
+    .read-more {
+      display: none;
+      transition: all 1s ease;
     }
   }
 `;
