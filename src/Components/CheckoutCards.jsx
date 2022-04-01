a4e4e7e6441d5513dcb0f4a0f867706bdc352c95
riteshsingla36 <ritesh.singla36@gmail.com>
2022-04-01 13:13:52 +0530

    minor database errors fixed

--- src/Components/CheckoutCards.jsx
+++ src/Components/CheckoutCards.jsx
@@ -1,67 +1,77 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import styles from './checkoutcard.module.css'
-import { EmptyBasketModel } from './EmptyBasketModel';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import styles from "./checkoutcard.module.css";
+import { EmptyBasketModel } from "./EmptyBasketModel";
 
 export const CheckoutCards = ({ obj }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  let subtotal = 0;
+  let categories = Object.keys(obj);
+  categories.forEach((key) => {
+    if (typeof obj[key] === typeof []) subtotal += obj[key].total;
+  });
 
-    let subtotal = 0;
-    let categories = Object.keys(obj)
-    categories.forEach((key) => {
-        if (typeof (obj[key]) === typeof ([])) subtotal += obj[key].total
-    })
-
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.buttondiv}>
-                {categories.length !== 0 ? <EmptyBasketModel/> : null}
-                <button onClick={()=> {
-                    navigate('/')
-                    window.scrollTo(0, 0);
-                    }}>CONTINUE SHOPPING</button>
+  return (
+    <div className={styles.container}>
+      <div className={styles.buttondiv}>
+        {categories.length !== 0 ? <EmptyBasketModel /> : null}
+        <button
+          onClick={() => {
+            navigate("/");
+            window.scrollTo(0, 0);
+          }}
+        >
+          CONTINUE SHOPPING
+        </button>
+      </div>
+      <div className={styles.checkoutcardDiv}>
+        {categories.length !== 0 ? (
+          <div className={styles.checkoutcard}>
+            <div className={styles.chargesDiv}>
+              <div className={styles.chargesSection1}>
+                <div>
+                  <p>Subtotal</p>
+                  <span>Rs. {subtotal.toFixed(2)}</span>
+                </div>
+                <div>
+                  <p>Delivery Charges</p>
+                  <span>**</span>
+                </div>
+                <div className={styles.totalDiv}>
+                  <p>TOTAL</p>
+                  <span>Rs. {subtotal.toFixed(2)}</span>
+                </div>
+              </div>
+              <div className={styles.savingDiv}>
+                <img
+                  src="https://www.bbassets.com/static/v2531/uiv2/css/images/your_saved_icon.png"
+                  alt=""
+                />
+                <span>You saved!</span>
+                <span>Rs. {obj.totalSaving.toFixed(2)}</span>
+              </div>
             </div>
-            <div className={styles.checkoutcardDiv}>
-                {categories.length !== 0 ?
-                    <div className={styles.checkoutcard}>
-                        <div className={styles.chargesDiv}>
-                            <div className={styles.chargesSection1}>
-                                <div>
-                                    <p>Subtotal</p>
-                                    <span>Rs. {subtotal}</span>
-                                </div>
-                                <div>
-                                    <p>Delivery Charges</p>
-                                    <span>**</span>
-                                </div>
-                                <div className={styles.totalDiv}>
-                                    <p>TOTAL</p>
-                                    <span>Rs. {subtotal}</span>
-                                </div>
-                            </div>
-                            <div className={styles.savingDiv}>
-                                <img src="https://www.bbassets.com/static/v2531/uiv2/css/images/your_saved_icon.png" alt="" />
-                                <span>You saved!</span>
-                                <span>Rs. {obj.totalSaving.toFixed(2)}</span>
-                            </div>
-                        </div>
-                        <div className={styles.coupondiv}>
-                            <p>* For this order: Accepted food coupon is {subtotal}</p>
-                        </div>
-                        <div className={styles.buttonSection}>
-                            <div>
-                                <button className={styles.checkoutButton}><p>CHECKOUT</p> <span></span></button>
-                            </div>
-                        </div>
-                        <div className={styles.warning}>
-                            <p>** Actual delivery charges computed at checkout time</p>
-                        </div>
-                    </div> : null}
+            <div className={styles.coupondiv}>
+              <p>
+                * For this order: Accepted food coupon is {subtotal.toFixed(2)}
+              </p>
             </div>
-        </div>
-    )
-}
+            <div className={styles.buttonSection}>
+              <div>
+                <button className={styles.checkoutButton}>
+                  <p>CHECKOUT</p> <span></span>
+                </button>
+              </div>
+            </div>
+            <div className={styles.warning}>
+              <p>** Actual delivery charges computed at checkout time</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
--- src/Components/Listitem.jsx
+++ src/Components/Listitem.jsx
@@ -1,144 +1,149 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { addQuantity, deleteItem, setCart } from '../Redux/cart/CartAction'
-import styles from './checkout.module.css'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addQuantity, deleteItem, setCart } from "../Redux/cart/CartAction";
+import styles from "./checkout.module.css";
 
-export const Listitem = ({item}) => {
+export const Listitem = ({ item }) => {
+  const dispatch = useDispatch();
+  const [value, setValue] = useState(item.quantity);
 
-    const dispatch = useDispatch();
-    const [value,setValue] = useState(item.quantity);
+  // const handleIncrease = () =>{
+  //     dispatch(addQuantity({id:item.id,quantity:item.quantity +1}))
+  // }
 
-    // const handleIncrease = () =>{
-    //     dispatch(addQuantity({id:item.id,quantity:item.quantity +1}))
+  // const handleDelete = () =>{
+  //     dispatch(deleteItem({id:item.id}))
+  // }
+
+  // const handleDecrease = () =>{
+  //     let quantity = item.quantity -1
+  //     if(quantity < 0){
+  //         quantity = 0
+  //     }
+  //     dispatch(addQuantity({id:item.id,quantity}))
+  // }
+
+  const handleIncrease = () => {
+    // dispatch(addQuantity({id:item.id,quantity:item.quantity +1}))
+    const updateQuantity = (id, newQty) => {
+      if (newQty === 0) {
+        axios
+          .delete(`/items/${id}`)
+          .then()
+          .catch((err) => alert(err.message));
+      } else {
+        axios
+          .patch(`/items/${id}`, { quantity: newQty })
+          .then()
+          .catch((err) => alert(err.message));
+      }
+    };
+
+    updateQuantity(item._id, item.quantity + 1);
+  };
+
+  const handleDelete = () => {
+    // dispatch(deleteItem({id:item.id}))
+    const deleteQuantity = (id) => {
+      axios
+        .delete(`/items/${id}`)
+        .then()
+        .catch((err) => alert(err.message));
+    };
+
+    deleteQuantity(item._id);
+  };
+
+  const handleDecrease = () => {
+    // let quantity = item.quantity -1
+    // if(quantity < 0){
+    //     quantity = 0
     // }
+    // dispatch(addQuantity({id:item.id,quantity}))
 
-    // const handleDelete = () =>{
-    //     dispatch(deleteItem({id:item.id}))
-    // }
+    const updateQuantity = (id, newQty) => {
+      if (newQty === 0) {
+        axios
+          .delete(`/items/${id}`)
+          .then()
+          .catch((err) => alert(err.message));
+      } else {
+        axios
+          .patch(`/items/${id}`, { quantity: newQty })
+          .then()
+          .catch((err) => alert(err.message));
+      }
+    };
 
-    // const handleDecrease = () =>{
-    //     let quantity = item.quantity -1
-    //     if(quantity < 0){
-    //         quantity = 0
-    //     }
-    //     dispatch(addQuantity({id:item.id,quantity}))
-    // }
+    updateQuantity(item._id, item.quantity - 1);
+  };
 
-    const handleIncrease = () =>{
-        // dispatch(addQuantity({id:item.id,quantity:item.quantity +1}))
-        const updateQuantity = (id, newQty) => {
-            if (newQty === 0) {
-              axios
-                .delete(`/items/${id}`)
-                .then()
-                .catch((err) => alert(err.message));
-            } else {
-              axios
-                .patch(`/items/${id}`, { quantity: newQty })
-                .then()
-                .catch((err) => alert(err.message));
-            }
-          };
-        
-        updateQuantity(item._id, item.quantity + 1)
-    }
+  useEffect(() => {
+    let quantity = value;
+    // dispatch(addQuantity({id:item.id,quantity}))
+    const updateQuantity = (id, newQty) => {
+      if (newQty === 0) {
+        axios
+          .delete(`/items/${id}`)
+          .then()
+          .catch((err) => alert(err.message));
+      } else {
+        axios
+          .patch(`/items/${id}`, { quantity: newQty })
+          .then()
+          .catch((err) => alert(err.message));
+      }
+    };
 
-    const handleDelete = () =>{
-        // dispatch(deleteItem({id:item.id}))
-        const deleteQuantity = (id) => {
-            axios
-                .delete(`/items/${id}`)
-                .then()
-                .catch((err) => alert(err.message));
-          };
-        
-        deleteQuantity(item._id)
-    }
+    updateQuantity(item._id, quantity);
+  }, [value]);
 
-    const handleDecrease = () =>{
-        // let quantity = item.quantity -1
-        // if(quantity < 0){
-        //     quantity = 0
-        // }
-        // dispatch(addQuantity({id:item.id,quantity}))
-
-        const updateQuantity = (id, newQty) => {
-            if (newQty === 0) {
-              axios
-                .delete(`/items/${id}`)
-                .then()
-                .catch((err) => alert(err.message));
-            } else {
-              axios
-                .patch(`/items/${id}`, { quantity: newQty })
-                .then()
-                .catch((err) => alert(err.message));
-            }
-          };
-        
-        updateQuantity(item._id, item.quantity - 1)
-    }
-
-    useEffect(() =>{
-        let quantity = value
-        // dispatch(addQuantity({id:item.id,quantity}))
-        const updateQuantity = (id, newQty) => {
-            if (newQty === 0) {
-              axios
-                .delete(`/items/${id}`)
-                .then()
-                .catch((err) => alert(err.message));
-            } else {
-              axios
-                .patch(`/items/${id}`, { quantity: newQty })
-                .then()
-                .catch((err) => alert(err.message));
-            }
-          };
-        
-        updateQuantity(item._id, quantity)
-    },[value])
-
-
-    useEffect(()=>{
-        const fetchcart = async() =>{
-          const res = await fetch('/items/?user=62435193c1dab43bbce3f6eb')
-          const json = await res.json();
-          dispatch(setCart(json))
-        }
-        fetchcart()
-      },[handleIncrease,handleDelete,handleDecrease,value])
-    
+  useEffect(() => {
+    const fetchcart = async () => {
+      const res = await fetch("/items/?user=62435193c1dab43bbce3f6eb");
+      const json = await res.json();
+      dispatch(setCart(json));
+    };
+    fetchcart();
+  }, [handleIncrease, handleDelete, handleDecrease, value]);
 
   return (
     <div className={styles.listItemDiv}>
-        <div>&nbsp;</div>
-        <div className={styles.description}>
-            <div className={styles.fade}>{item.product.brand}</div>
-            <div>{item.product.name}</div>
+      <div>&nbsp;</div>
+      <div className={styles.description}>
+        <div className={styles.fade}>{item.product.brand}</div>
+        <div>{item.product.name}</div>
+      </div>
+      <div>
+        <div>Rs: {item.product.price}</div>
+        <div className={styles.fade}>
+          <strike>Rs: {item.product.mrp}</strike>
         </div>
-        <div>
-            <div>Rs: {item.product.price}</div>
-            <div className={styles.fade}><strike>Rs: {item.product.mrp}</strike></div>
+      </div>
+      <div>
+        <div className={styles.quantitydiv}>
+          <button onClick={handleDecrease}>-</button>
+          <input
+            type={"number"}
+            id={item.id}
+            value={item.quantity}
+            min={"1"}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button onClick={handleIncrease}>+</button>
         </div>
-        <div>
-            <div className={styles.quantitydiv}>
-                <button onClick={handleDecrease}>-</button>
-                <input type={"number"} id={item.id} value={item.quantity}  min={"1"} onChange={(e)=> setValue(e.target.value)}/>
-                <button onClick={handleIncrease}>+</button>
-            </div>
-        </div>
-        <div>
-            <div>Rs. {item.subtotal}</div>
-        </div>
-        <div className={styles.crossSign} onClick={handleDelete} >
-            <img src="https://www.bigbasket.com/static/uiv2/css/images/close_grid_icon.png" />
-        </div>
-        <div>
-            <div className={styles.savings}>Rs. {item.saving.toFixed(2)}</div>
-        </div>
+      </div>
+      <div>
+        <div>Rs. {item.subtotal.toFixed(2)}</div>
+      </div>
+      <div className={styles.crossSign} onClick={handleDelete}>
+        <img src="https://www.bigbasket.com/static/uiv2/css/images/close_grid_icon.png" />
+      </div>
+      <div>
+        <div className={styles.savings}>Rs. {item.saving.toFixed(2)}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
